8b2ddf92cd0a62d25f27d6375c3c28c63668e8f6
olde0304 <oldemar414@gmail.com>
2021-11-17 20:54:09 -0600

    combo listi

---
+++ src/components/Criptomonedas.js
@@ -0,0 +1,158 @@
+export const critomendas = {
+    "777": {
+        "is_fiat": 0,
+        "rate_btc": "0.000000850569151603920000",
+        "last_update": "1636990862",
+        "tx_fee": "0.00000000",
+        "status": "online",
+        "name": "BINGO Entertainment",
+        "confirms": "3",
+        "can_convert": 0,
+        "capabilities": [
+            "wallet"
+        ],
+        "chain": "ETH",
+        "contract": "0xd8002cd05d5b2a85557e1caaa179cc2408d5ad42",
+        "explorer": "https://etherscan.io/tx/%txid%",
+        "accepted": 0
+    },
+    "BTC": {
+        "is_fiat": 0,
+        "rate_btc": "1.000000000000000000000000",
+        "last_update": "1375473661",
+        "tx_fee": "0.00040000",
+        "status": "online",
+        "name": "Bitcoin",
+        "confirms": "2",
+        "can_convert": 1,
+        "capabilities": [
+            "payments",
+            "wallet",
+            "transfers",
+            "convert"
+        ],
+        "explorer": "https://blockchair.com/bitcoin/transaction/%txid%?from=coinpayments",
+        "accepted": 1
+    },
+    "BTC.BEP2": {
+        "is_fiat": 0,
+        "rate_btc": "1.000000000000000000000000",
+        "last_update": "1636990862",
+        "tx_fee": "0.00000000",
+        "status": "online",
+        "name": "Bitcoin/BTCB Token (BC Chain)",
+        "confirms": "10",
+        "can_convert": 1,
+        "capabilities": [
+            "payments",
+            "wallet",
+            "dest_tag",
+            "convert"
+        ],
+        "chain": "BNB",
+        "contract": "BTCB-1DE",
+        "explorer": "https://explorer.binance.org/tx/%txid%",
+        "accepted": 0
+    },
+    "BTC.BEP20": {
+        "is_fiat": 0,
+        "rate_btc": "1.000000000000000000000000",
+        "last_update": "1636990862",
+        "tx_fee": "0.00000000",
+        "status": "online",
+        "name": "Bitcoin/BTCB Token (BSC Chain)",
+        "confirms": "50",
+        "can_convert": 1,
+        "capabilities": [
+            "payments",
+            "wallet",
+            "convert"
+        ],
+        "chain": "BNB.BSC",
+        "contract": "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c",
+        "explorer": "https://bscscan.com/tx/%txid%",
+        "accepted": 0
+    },
+    "BTC.LN": {
+        "is_fiat": 0,
+        "rate_btc": "1.000000000000000000000000",
+        "last_update": "2147483647",
+        "tx_fee": "0.00002500",
+        "status": "online",
+        "name": "Bitcoin (Lightning Network)",
+        "confirms": "1",
+        "can_convert": 0,
+        "capabilities": [
+            "payments"
+        ],
+        "accepted": 0
+    },
+    "USD": {
+        "is_fiat": 1,
+        "rate_btc": "0.000015519443873377000000",
+        "last_update": "1636990862",
+        "tx_fee": "0.00000000",
+        "status": "online",
+        "name": "United States Dollar",
+        "confirms": "1",
+        "can_convert": 0,
+        "capabilities": [],
+        "accepted": 0
+    },
+    "BCH": {
+        "is_fiat": 0,
+        "rate_btc": "0.010379012844724000000000",
+        "last_update": "1636990862",
+        "tx_fee": "0.00300000",
+        "status": "online",
+        "name": "Bitcoin Cash",
+        "confirms": "3",
+        "can_convert": 1,
+        "capabilities": [
+            "payments",
+            "wallet",
+            "transfers",
+            "convert"
+        ],
+        "explorer": "https://blockchair.com/bitcoin-cash/transaction/%txid%?from=coinpayments",
+        "accepted": 0
+    },
+    "BCH.BEP2": {
+        "is_fiat": 0,
+        "rate_btc": "0.010367163211181000000000",
+        "last_update": "1636990862",
+        "tx_fee": "0.00000000",
+        "status": "online",
+        "name": "Bitcoin Cash Token (BC Chain)",
+        "confirms": "10",
+        "can_convert": 0,
+        "capabilities": [
+            "payments",
+            "wallet",
+            "dest_tag"
+        ],
+        "chain": "BNB",
+        "contract": "BCH-1FD",
+        "explorer": "https://explorer.binance.org/tx/%txid%",
+        "accepted": 0
+    },
+    "BCH.BEP20": {
+        "is_fiat": 0,
+        "rate_btc": "0.010367163211181000000000",
+        "last_update": "1636990862",
+        "tx_fee": "0.00000000",
+        "status": "online",
+        "name": "Bitcoin Cash Token (BSC Chain)",
+        "confirms": "50",
+        "can_convert": 1,
+        "capabilities": [
+            "payments",
+            "wallet",
+            "convert"
+        ],
+        "chain": "BNB.BSC",
+        "contract": "0x8ff795a6f4d97e7887c79bea79aba5cc76444adf",
+        "explorer": "https://bscscan.com/tx/%txid%",
+        "accepted": 0
+    }
+}
--- src/components/PurcoApp/PantallaUno.js
+++ src/components/PurcoApp/PantallaUno.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Flechas } from './Flechas'
-
+import { critomendas } from '../Criptomonedas';
+import { useForm } from '../../hooks/useForm';
 
 export const PantallaUno = () => {
+
+    const moneda = Object.values(critomendas)
+
+    const [formValues, handleInputChange] = useForm(
+        {
+            monedaSeleccionada: ''
+        }
+    )
+    
+    const {monedaSeleccionada} = formValues;
+    
+    console.log(monedaSeleccionada)
 
     return (
         <>
@@ -10,6 +23,20 @@ export const PantallaUno = () => {
 
             <div className="contenedorCentral">
                 <p className="contenedorCentral__text">aqui va toda la logica del proceso de pago 1</p>
+
+                <div className="contendorComboBox">
+                    <label className="">Critomonedas</label>
+                    <select name="monedaSeleccionada" onClick={handleInputChange} className="selector" >
+                        <option value={monedaSeleccionada}>Selecione</option>
+                        {
+                            moneda.map((m, i) => (
+                                <option key={i} value={m.name}>{m.name}</option>
+                            ))
+                        }
+                    </select>
+                </div>
+
+
             </div>
         </>
     )
